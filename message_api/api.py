c467600e1f623732c9c656224e899af9c4a6cefa
Nurul <erkinkazhy.n@gmail.com>
2026-05-14 22:08:38 +0500

    first commit

--- message_api/api.py
+++ message_api/api.py
@@ -6,7 +6,7 @@ from langbly import Langbly
 class chat_api:
     def send_message(message:str):
         message = message.strip()
-        if message.isdigit() == True or message.isalnum() == False or message.isspace():
+        if message.isdigit() == True or message.isspace() == True or message.isalpha() == 0:
             return []
         trans = Langbly(api_key="PN4oc9vZk1zMXaoJsMTyJE")
         result = trans.translate(
